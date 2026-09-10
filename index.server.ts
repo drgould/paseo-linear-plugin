a83d0ae5cf756e6cadff6feb65016782b14a53c2
Derek Gould <derek.gould@ent.ai>
2026-09-10 11:23:29 -0600

    chore: scaffold Paseo Linear plugin

---
+++ index.server.ts
@@ -0,0 +1,8 @@
+import type { PluginServerContext } from "@getpaseo/plugin/server";
+
+export default function contribute(server: PluginServerContext) {
+  // TODO(issue 2): server.handle(searchIssuesRpc, searchIssues);
+  // TODO(issue 3): server.handle(myIssuesRpc, listMyIssues);
+  // TODO(issue 4): server.handle(issueDetailRpc, getIssueDetail);
+  return () => {};
+}
